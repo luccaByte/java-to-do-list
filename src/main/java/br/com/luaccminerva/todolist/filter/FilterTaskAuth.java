89ba5b8c16c96194a82304b01b7aaa62c0dcedfe
lucas <lucaston2323@gmail.com>
2023-10-12 14:40:12 -0300

    Construindo validação de dateTime para o to-do list

--- src/main/java/br/com/luaccminerva/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/luaccminerva/todolist/filter/FilterTaskAuth.java
@@ -22,42 +22,49 @@ public class FilterTaskAuth extends OncePerRequestFilter{
     private IUserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        
-                // Pegar a autenticação (usuario e senha)
-                var authorization = request.getHeader("Authorization");
-                // remove a palavra basic e pega somente o auth, e após remove o espaço em branco com trim
-                var authEncoded = authorization.substring("Basic".length()).trim(); 
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
+        throws ServletException, IOException {
 
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+        var servletPath = request.getServletPath();
 
-                var authString = new String (authDecode);
+        if (servletPath.equals("/tasks/")) {
+            // Pegar a autenticação (usuario e senha)
+            var authorization = request.getHeader("Authorization");
+            // remove a palavra basic e pega somente o auth, e após remove o espaço em branco com trim
+            var authEncoded = authorization.substring("Basic".length()).trim(); 
 
-                // split cria um array colocando o usuario na posição 0, e a senha na posição 1
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
-                System.out.println("Authorization");
-                System.out.println(username);
-                System.out.println(password);
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-                // Validar usuario
-                var user = this.userRepository.findByUsername(username);
-                if (user == null) {
-                    response.sendError(401);
+            var authString = new String (authDecode);
+
+            // split cria um array colocando o usuario na posição 0, e a senha na posição 1
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+            System.out.println("Authorization");
+            System.out.println(username);
+            System.out.println(password);
+
+            // Validar usuario
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                // Validar senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
                 } else {
-                    // Validar senha
-                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-                    if (passwordVerify.verified) {
-                        filterChain.doFilter(request, response);
-                    } else {
-                        response.sendError(401);
-                    }
-
-                    // Segue viagem
+                    response.sendError(401);
                 }
 
+                // Segue viagem
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+                
     }
 
 }
--- src/main/java/br/com/luaccminerva/todolist/task/TaskController.java
+++ src/main/java/br/com/luaccminerva/todolist/task/TaskController.java
@@ -1,12 +1,19 @@
 package br.com.luaccminerva.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.var;
+import jakarta.servlet.http.HttpServletRequest;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -16,8 +23,23 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        System.out.println("Chegou no controller " + request.getAttribute("idUser"));
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        
+        var currentDate = LocalDateTime.now();
+        // 10-11-2023 current
+        // 10-10-2023 start
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / data de término deve ser maior do que a data atual.");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor do que a data de término..");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
